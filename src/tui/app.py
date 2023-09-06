b73eafcf51f18c05ee7e5b71e3b73d5a441fe60a
Daniel Caballero <danielcaballero88@gmail.com>
2023-09-06 07:53:54 +0200

    Styling

--- src/tui/app.py
+++ src/tui/app.py
@@ -1,28 +1,30 @@
 from textual.app import App, ComposeResult
 from textual.containers import ScrollableContainer
-from textual.widgets import Button, Footer, Header, Pretty, Static
+from textual.widgets import Button, Footer, Header, Static
 
 
 class LoginBox(Static):
     def compose(self) -> ComposeResult:
         yield Button("Log in", id="login-button", variant="success")
-        yield Pretty({"a": 1, "b": 2}, id="login-info")
 
 
 class ConnectBox(Static):
     def compose(self) -> ComposeResult:
-        yield Pretty({"a": 1, "b": 2}, id="login-info")
         yield Button("Connect", id="connect-button", variant="success")
 
 
 class StatusHeader(Static):
     def compose(self) -> ComposeResult:
-        yield LoginBox()
-        yield ConnectBox()
+        yield LoginBox(classes="button-box")
+        yield ConnectBox(classes="button-box")
 
 
 class NordvpnTUI(App):
-    CSS_PATH = "placeholder.tcss"
+    CSS_PATH = "app.tcss"
+    BINDINGS = [
+        ("d", "toggle_dark", "Toggle dark mode"),
+        ("q", "quit", "Quit"),
+    ]
 
     def compose(self) -> ComposeResult:
         yield Header()
