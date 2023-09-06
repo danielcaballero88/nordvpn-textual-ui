9fe4578ce5286e3144b8532540673cf99cf8dc2e
Daniel Caballero <danielcaballero88@gmail.com>
2023-09-06 08:03:40 +0200

    Add modal when requestiong to quit

--- src/tui/app.py
+++ src/tui/app.py
@@ -1,6 +1,25 @@
 from textual.app import App, ComposeResult
-from textual.containers import ScrollableContainer
-from textual.widgets import Button, Footer, Header, Static
+from textual.containers import Grid, ScrollableContainer
+from textual.screen import Screen
+from textual.widgets import Button, Footer, Header, Label, Static
+
+
+class QuitScreen(Screen):
+    """Screen with a dialog to quit."""
+
+    def compose(self) -> ComposeResult:
+        yield Grid(
+            Label("Are you sure you want to quit?", id="question"),
+            Button("Quit", variant="error", id="quit"),
+            Button("Cancel", variant="primary", id="cancel"),
+            id="dialog",
+        )
+
+    def on_button_pressed(self, event: Button.Pressed) -> None:
+        if event.button.id == "quit":
+            self.app.exit()
+        else:
+            self.app.pop_screen()
 
 
 class LoginBox(Static):
@@ -23,13 +42,16 @@ class NordvpnTUI(App):
     CSS_PATH = "app.tcss"
     BINDINGS = [
         ("d", "toggle_dark", "Toggle dark mode"),
-        ("q", "quit", "Quit"),
+        ("q", "request_quit", "Quit"),
     ]
 
     def compose(self) -> ComposeResult:
         yield Header()
         yield Footer()
         yield ScrollableContainer(StatusHeader())
+
+    def action_request_quit(self) -> None:
+        self.push_screen(QuitScreen())
 
 
 if __name__ == "__main__":
